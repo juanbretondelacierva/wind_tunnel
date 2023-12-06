90fa7bdaac5fda52778bbd4decd46e1cb193e036
juanbretondelacierva <150688836+juanbretondelacierva@users.noreply.github.com>
2023-12-06 20:09:56 +0100

    señor subnautica

--- 2d_temp_analysis.py
+++ 2d_temp_analysis.py
@@ -1,3 +1,11 @@
 import numpy as np
-data_array = np.loadtxt(open("2D/0/1sigma_2023-12-01_09-22-11.csv", "rb"), delimiter=";", usecols=range(640))
-print(data_array[1][0])
+import os
+
+data_matrix = np.zeros((480, 640))
+data_matrix = np.asmatrix(data_matrix)
+for filename in os.listdir("2D/0/"):
+    number_of_files = len([name for name in os.listdir("2D/0/") if os.path.isfile(os.path.join("2D/0/", name))])
+    data_array = np.matrix(np.loadtxt(open("2D/0/"+filename, "rb"), delimiter=";", usecols=range(640)))
+    data_matrix += data_array/number_of_files
+
+    print(data_matrix)
