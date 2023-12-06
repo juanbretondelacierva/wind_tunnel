f371c16d24ac92ae0eba01e2bdf39065510ed529
juanbretondelacierva <150688836+juanbretondelacierva@users.noreply.github.com>
2023-12-06 19:34:58 +0100

    soy el puto amo

--- 2d_temp_analysis.py
+++ 2d_temp_analysis.py
@@ -1,6 +1,3 @@
-import pandas as pd
-df = pd.read_excel(io="2D/0/1sigma_2023-12-01_09-22-11.csv", sheet_name="1sigma_2023-12-01_09-22-11")
-
-a = df.as_matrix()
-
-print(df.head(5))
+import numpy as np
+data_array = np.loadtxt(open("2D/0/1sigma_2023-12-01_09-22-11.csv", "rb"), delimiter=";", usecols=range(640))
+print(data_array[1][0])
