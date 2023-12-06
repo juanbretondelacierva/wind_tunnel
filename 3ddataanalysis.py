024285530aa8a50813cb5c23a5f07210a801e490
Jaime <jaimedesalasjuanas@gmail.com>
2023-12-06 20:21:27 +0100

    Dict with all data

--- 3ddataanalysis.py
+++ 3ddataanalysis.py
@@ -1,11 +1,29 @@
 import numpy as np
 import os
+import copy
+import matplotlib.pyplot as plt
 
 #data = [np.loadtxt(f'\\3D\\{i}') for i in range(-3, 13)]
 
-data = []
-for i, filename in enumerate(os.listdir('.\\3D\\0')):
-   with open(os.path.join('.\\3D\\0', filename)) as f: # open in readonly mode
-      data.append(np.loadtxt(f, delimiter=';', usecols=range(640)))
+# Initialize dictionary with keys as folder names
+data = {key:[] for key in os.listdir('.\\3D')}
 
-print(data[0])
+for directory in os.listdir('.\\3D'):
+    #folderarray = np.zeros(len(os.listdir(f'.\\3D\\{directory}')))
+    folderarray = np.asmatrix(np.zeros((480, 640)))
+    print(f'Current directory: {directory}')
+
+    for i, filename in enumerate(os.listdir(f'.\\3D\\{directory}')):
+        with open(os.path.join(f'.\\3D\\{directory}', filename)) as f: # open in readonly mode
+            folderarray += np.matrix(np.loadtxt(f, delimiter=';', usecols=range(640)))
+    
+    folderarray /= (i+1)
+    data[directory] = copy.deepcopy(folderarray)
+
+
+#print(data['0'])
+fig, ax = plt.subplots()
+im = ax.imshow(data['0'])
+fig.colorbar(im, ax=ax, label='Interactive colorbar')
+
+plt.show()
