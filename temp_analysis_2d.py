72f2369b0fb603ef0b60069140b4d629208354e8
juanbretondelacierva <150688836+juanbretondelacierva@users.noreply.github.com>
2023-12-07 14:17:12 +0100

    Update temp_analysis_2d.py

--- temp_analysis_2d.py
+++ temp_analysis_2d.py
@@ -2,8 +2,8 @@ import numpy as np
 import os
 from matplotlib import pyplot as plt
 
-#aoa = ["-3","-2","-1","0","1","2","3","3.5","4","4.5","5","5.5","6","6.5","7","7.5","8","8.5","9","9.5","10","10.5","11","11.5","12","12.5","13","13.5","14","14.5","15","15.5","16","16.5","17","17.5","18","17.5h","17h","16.5h","16h","15.5h","15h","14.5h","14h","13.5h","13h","12.5h","12h","11.5h"]
-aoa = ["2"]
+aoa = ["-3","-2","-1","0","1","2","3","3.5","4","4.5","5","5.5","6","6.5","7","7.5","8","8.5","9","9.5","10","10.5","11","11.5","12","12.5","13","13.5","14","14.5","15","15.5","16","16.5","17","17.5","18","17.5h","17h","16.5h","16h","15.5h","15h","14.5h","14h","13.5h","13h","12.5h","12h","11.5h"]
+#aoa = ["6"]
 
 right_limit = 446
 left_limit = 196
@@ -28,29 +28,34 @@ def row_to_image(array):
 def derivate_row(array):
     return np.append(np.diff(array), np.mean(np.diff(array)))
 
-def find_line(array):
+def find_line(array, id):
     pixelated_row = np.zeros(50)
-    index = np.argmax(np.abs(array))
+    if id >= 18 and id != 44 and id != 46:
+        index = np.argmax(np.abs(array))
+    else:
+        index = np.argmax(array)
     pixelated_row[index]=1
     return pixelated_row
 
 def plot():
+    i = 0
     for folder in aoa:
         data_matrix = np.asmatrix(np.zeros((right_limit-left_limit, right_limit-left_limit)))
         for filename in os.listdir("2D/"+str(folder)+"/"):
             number_of_files = len([name for name in os.listdir("2D/"+str(folder)+"/") if os.path.isfile(os.path.join("2D/"+str(folder)+"/", name))])
             data_array = np.matrix(np.loadtxt(open("2D/"+str(folder)+"/"+filename, "rb"), delimiter=";", usecols=range(left_limit, right_limit), skiprows=140, max_rows=(right_limit-left_limit)))
             data_matrix += data_array/number_of_files
-        
-        fig, ax = plt.subplots(1,2)
-        fig.suptitle(f"Angle of Attack "+str(folder))
-        im = ax[0].imshow(data_matrix, cmap="jet")
-        fig.colorbar(im, ax=ax, label='Interactive colorbar')
-        #im = ax[1].imshow(row_to_image(derivate_row(pixelate_columns(average_columns(data_matrix)))), cmap="jet")
-        #fig.colorbar(im, ax=ax, label='Interactive colorbar')
-        im = ax[1].imshow(row_to_image(find_line(derivate_row(pixelate_columns(average_columns(data_matrix))))), cmap="binary")
-        fig.colorbar(im, ax=ax, label='Interactive colorbar')
-        plt.show()
+        if i >= 42:
+            fig, ax = plt.subplots(1,2)
+            fig.suptitle(f"Angle of Attack "+str(folder))
+            im = ax[0].imshow(data_matrix, cmap="jet")
+            fig.colorbar(im, ax=ax, label='Interactive colorbar')
+            #im = ax[1].imshow(row_to_image(derivate_row(pixelate_columns(average_columns(data_matrix)))), cmap="jet")
+            #fig.colorbar(im, ax=ax, label='Interactive colorbar')
+            im = ax[1].imshow(row_to_image(find_line(derivate_row(pixelate_columns(average_columns(data_matrix))), i)), cmap="binary")
+            fig.colorbar(im, ax=ax, label='Interactive colorbar')
+            plt.show()
+        i += 1
     
 
 plot()
