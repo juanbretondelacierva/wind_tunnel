208f48ac502747ca85c05b43df90110f8ad7d9cd
Jaime <jaimedesalasjuanas@gmail.com>
2023-12-07 11:22:47 +0100

    Works

--- dataloading_3d.py
+++ dataloading_3d.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 import copy
+import pickle
 
 data = {key:None for key in os.listdir('.\\3D')}
 
@@ -14,9 +15,13 @@ if __name__ == '__main__':
 
         for i, filename in enumerate(os.listdir(f'.\\3D\\{directory}')):
             with open(os.path.join(f'.\\3D\\{directory}', filename)) as f: # open in readonly mode
-                folderarray += np.array(np.loadtxt(f, delimiter=';', usecols=range(640)))
+                folderarray += np.asarray(np.loadtxt(f, delimiter=';', usecols=range(640)))
         
         folderarray /= (i+1)
+        print(type(folderarray))
         data[directory] = copy.deepcopy(folderarray)
 
     print('Data Gatehred')
+
+with open('saved_variables_3d.pkl', 'wb') as file:
+    pickle.dump(data, file)
