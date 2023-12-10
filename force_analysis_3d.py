79a677ee0540bede04b0c13e88227ee32a4c849a
juanbretondelacierva <150688836+juanbretondelacierva@users.noreply.github.com>
2023-12-10 14:23:01 +0100

    He perdido sin quererlo los papeles que me diste antes de ayer Donde estaban los consejos que apuntamos pa'que todo fuera bien Y ahora estamos camino de la frontera disfrutando a poquitos la vida entera Así que tengo que encontrarte para verte y que me digas otra vez  Y necesito una ayudita una palabra que me pueda convencer Y cuando me hablas la montaña es más pequeña y no se mueve cada vez Que dice que cruzamos camino de la frontera disfrutando a sorbitos la luna llena Como no voy a mojarme si aquí dentro nunca deja de llover aquí no para de llover  Y si seguimos con el plan establecido Nos cansaremos al ratito de empezar Probablemente no encontremos el camino Pero nos sobrarán las ganas de volar nuestras ganas de volar  Y que fácil es perderse de la mano madre mía agárrate Que el vacío de ese vaso no se llena si no vuelves tú a querer (Jerez) Y pasa cuando estamos camino de la frontera pobrecita cansada la vida queda Como no voy a cansarla si no paro y nunca dejo de correr Y si no paro de correr  Improvisemos un guión definitivo Que no tengamos más remedio que olvidar Que acerque todas las estrellas al camino Para que nunca falten ganas de soñar Y suena bien, parece que nos hemos convencido Solo tenemos que perder velocidad Hace ya tiempo que no estamos divididos Algo sobraba cuando echamos a volar cuando echamos a volar Y sé que sé, que suena diferente En tu futuro en su pasado en mi presente  Y hemos sobrevivido aunque no sé bien a que Y es que andábamos tan perdidos que no podíamos ver La alegría que se lleva el miedo Los buenos ratos el sol de enero Ver contigo cada amanecer Pensando dan bi dan bi dan Dan bi dan bi dan  Cuenta hasta tres, empiezo yo primero Y así el efecto del disparo es más certero Ya no sigas tú quitándole la prisa, mirando como la tortuga te hipnotiza Y nadie se hará el camino sin suerte, que aquí lo malo en bueno se convierte Existe un sendero y te has convencido, así que empiézalo conmigo y echaremos a volar Y echaremos a volar  Nadie se haré el camino sin suerte Que aquí la pena en pedacitos se convierte Te aguarda un mundo entero, titiritero No lo hagas esperar
    same for 3d, missing the cm diagram but i can't be bothered rn

--- force_analysis_3d.py
+++ force_analysis_3d.py
@@ -1,1 +1,94 @@
 
+import numpy as np
+import os
+from matplotlib import pyplot as plt
+
+def cl_alpha():
+    data_array = np.loadtxt(open("Forces/3D/corr_test.txt", "rb"), delimiter="	", usecols=[1,3], skiprows=2)
+    alpha = data_array[:, 0]
+    cl = data_array[:, 1]
+
+    fig, ax = plt.subplots()
+    ax.plot(alpha, cl, "o", color="blue", markersize=4)
+    ax.plot(alpha, cl, "-", color="blue", label="Corrected Cl-alpha")
+
+    data_array = np.loadtxt(open("Forces/3D/unc_test.txt", "rb"), delimiter="	", usecols=[1,3], skiprows=2)
+    alpha = data_array[:, 0]
+    cl = data_array[:, 1]
+
+    ax.plot(alpha, cl, "o", color="orange", markersize=4)
+    ax.plot(alpha, cl, "-", color="orange", label="Uncorrected Cl-alpha")
+
+    ax.set_xlabel('Alpha')
+    ax.set_ylabel('Cl')
+    plt.legend()
+    plt.show()
+
+def cd_alpha():
+    data_array = np.loadtxt(open("Forces/3D/corr_test.txt", "rb"), delimiter="	", usecols=[1,4], skiprows=2)
+    alpha = data_array[:, 0]
+    cd = data_array[:, 1]
+
+    fig, ax = plt.subplots()
+    ax.plot(alpha, cd, "o", color="blue", markersize=4)
+    ax.plot(alpha, cd, "-", color="blue", label="Corrected Cd-alpha")
+
+    data_array = np.loadtxt(open("Forces/3D/unc_test.txt", "rb"), delimiter="	", usecols=[1,4], skiprows=2)
+    alpha = data_array[:, 0]
+    cd = data_array[:, 1]
+
+    ax.plot(alpha, cd, "o", color="orange", markersize=4)
+    ax.plot(alpha, cd, "-", color="orange", label="Uncorrected Cd-alpha")
+
+    ax.set_xlabel('Alpha')
+    ax.set_ylabel('Cd')
+    plt.legend()
+    plt.show()
+
+def cl_cd():
+    data_array = np.loadtxt(open("Forces/3D/corr_test.txt", "rb"), delimiter="	", usecols=[4,3], skiprows=2)
+    cd = data_array[:, 0]
+    cl = data_array[:, 1]
+
+    fig, ax = plt.subplots()
+    ax.plot(cd, cl, "o", color="blue", markersize=4)
+    ax.plot(cd, cl, "-", color="blue", label="Corrected Cl-Cd")
+
+    data_array = np.loadtxt(open("Forces/3D/unc_test.txt", "rb"), delimiter="	", usecols=[4,3], skiprows=2)
+    cd = data_array[:, 0]
+    cl = data_array[:, 1]
+
+    ax.plot(cd, cl, "o", color="orange", markersize=4)
+    ax.plot(cd, cl, "-", color="orange", label="Uncorrected Cl-Cd")
+
+    ax.set_xlabel('Cd')
+    ax.set_ylabel('Cl')
+    plt.legend()
+    plt.show()
+
+def cp_dist():
+    x = np.loadtxt(open("Forces/3D/cp_test.txt", "rb"), delimiter="	", usecols=0, skiprows=2)
+    fig, ax = plt.subplots()
+    for i in range(0, 53, 1):
+        if i == 6 or i ==8:
+            continue
+        else:
+            if i == 47 or i == 29:
+                cp = np.loadtxt(open("Forces/3D/cp_test.txt", "rb"), delimiter="	", usecols=(i+1), skiprows=2)
+                alpha = np.loadtxt(open("Forces/3D/cp_test.txt", "rb"), delimiter="	", usecols=(i+1), skiprows=1, max_rows=1)
+                ax.plot(x, cp, "o", markersize=2)
+                if i <= 38:
+                    ax.plot(x, cp, "-", color="blue", label=("Cd - "+str(alpha)+"º"))
+                else:
+                    ax.plot(x, cp, "-", color="orange", label=("Cd - "+str(alpha)+"º H"))
+
+
+    ax.set_xlabel('Cd')
+    ax.set_ylabel('X')
+    plt.legend(ncol=5)
+    plt.show()
+    
+
+cl_alpha()
+cl_cd()
+cd_alpha()
