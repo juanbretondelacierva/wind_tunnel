3ea65812f1700abcbb8509761fe184f2a954e601
Jan Franquesa <janfranquesam@gmail.com>
2023-12-18 13:04:40 +0100

    cambios de polar graphs

--- plot3Dnumerical.py
+++ plot3Dnumerical.py
@@ -9,7 +9,7 @@ def convertarrayandplot(data, label = ''):
     return
 
 for filename in os.listdir('.\\Numerical3D'):
-    data_LTT = np.loadtxt(open(os.path.join('.\\Numerical3D', filename), "rb"), delimiter=",", usecols=[0, 1], skiprows=1)
+    data_LTT = np.loadtxt(open(os.path.join('.\\Numerical3D', filename), "rb"), delimiter=",", usecols=[0, 1], skiprows=1, max_rows=42)
     data_Panel = np.loadtxt(open(os.path.join('.\\Numerical3D', filename), "rb"), delimiter=",", usecols=[3, 4], skiprows=1)
     data_VLM = np.loadtxt(open(os.path.join('.\\Numerical3D', filename), "rb"), delimiter=",", usecols=[6, 7], skiprows=1)
 
