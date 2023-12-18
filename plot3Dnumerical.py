1266c5f5d24ebbc94cb8accd91c4771b64bbe091
Jan Franquesa <janfranquesam@gmail.com>
2023-12-18 15:24:16 +0100

    plot corrections

--- plot3Dnumerical.py
+++ plot3Dnumerical.py
@@ -4,16 +4,9 @@ import os
 
 
 
-zticks=[]
-i=-3
-while i<=18:
-    zticks+=[i]
-    i+=0.5
-
-
-def convertarrayandplot(data, label = ''):
+def convertarrayandplot(data, trace, dot, label = ''):
     x, y = [item[0] for item in data], [item[1] for item in data]
-    plt.plot(x, y, marker='.', label=label)
+    plt.plot(x, y, linestyle=trace ,marker=dot, label=label)
     return
 
 
@@ -23,9 +16,10 @@ for filename in os.listdir('.\\Numerical3D'):
     data_Panel = np.loadtxt(open(os.path.join('.\\Numerical3D', filename), "rb"), delimiter=",", usecols=[3, 4], skiprows=1)
     data_VLM = np.loadtxt(open(os.path.join('.\\Numerical3D', filename), "rb"), delimiter=",", usecols=[6, 7], skiprows=1)
 
-    convertarrayandplot(data_LTT, label = 'LTT')
-    convertarrayandplot(data_Panel, label = 'Panel Method')
-    convertarrayandplot(data_VLM, label = 'VLM')
+    convertarrayandplot(data_LTT, "-",".", label = 'LTT')
+    convertarrayandplot(data_Panel, ":","", label = 'Panel Method')
+    convertarrayandplot(data_VLM, ":","", label = 'VLM')
+    
     
     plt.title(f"{filename}")
     plt.grid()
--- plot3Dnumerical3Dgraphs.py
+++ plot3Dnumerical3Dgraphs.py
@@ -4,21 +4,23 @@ import colorsys
 import os
 
 
-def convertarrayandplot(data,z, label = ''):
-    x, y = [item[0] for item in data], [item[1] for item in data]
-    plt.plot(x, y, z, marker='.', label=label)
-    return
+
 
 def generate_rainbow_colors(num_colors):
     colors = []
     for i in range(num_colors):
         hue = i / num_colors
         rgb = colorsys.hsv_to_rgb(hue, 1, 1)
-        colors.append(tuple(int(c * 255) for c in rgb))
+        colors.append(tuple(float(c * 1) for c in rgb))
     return colors
 
 num_colors = 42
 colours = generate_rainbow_colors(num_colors)
+
+def convertarrayandplot(data,z, c, label = ''):
+    x, y = [item[0] for item in data], [item[1] for item in data]
+    plt.plot(x, y, z, marker='.', label=label, color=c)
+    return
 
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
@@ -32,7 +34,7 @@ def get_graph(order):
     for filename in os.listdir('.\\NumericalTransition3D'):
         if str(filename)[0:-4]==order:
             data = np.loadtxt(open(os.path.join('.\\NumericalTransition3D', filename), "rb"), delimiter=",", usecols=[0, 1],skiprows=1)
-            convertarrayandplot(data,float(order),label= order)
+            convertarrayandplot(data,float(order),colours[zticks.index(float(order))],label= order)
     return 0
 
 
@@ -40,4 +42,5 @@ for i in zticks:
     get_graph(str(i))
 
 ax.set_zticks(zticks)
+ax.set_xscale("linear")
 plt.show()
